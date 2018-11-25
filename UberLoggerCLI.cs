20cdd83b75e34f1a062f0be4b67d2f72a1ebfd3c
Ewan Mellor <commits@ewanmellor.org>
2018-11-24 23:46:55 -0800

    Extend UberLoggerCLI to add subcommands.
    Extend the UberLoggerCLI mechanism to add subcommands, so that callers can register
    the full command tree here, and we can do usage messages and complete dispatch
    without callers having to do it.

--- UberLoggerCLI.cs
+++ UberLoggerCLI.cs
@@ -1,37 +1,170 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
 using UnityEngine;
+using UnityEngine.Assertions;
 
 
-public sealed class UberLoggerCLI : MonoBehaviour {
+public sealed class UberLoggerCLI : MonoBehaviour
+{
 
     public const string TextFieldName = "UberLoggerCLI_TextField";
     const string CallbackMethodName = "UberLoggerCLI_HandleCommand";
 
-    readonly Dictionary<string, Component> handlers = new Dictionary<string, Component>();
+
+    public interface IHandler
+    {
+        string UberLoggerCLI_CommandName { get; }
+
+        /// <summary>
+        /// May be null or empty, in which case this command is a leaf and
+        /// uses UberLoggerCLI_Action instead.
+        /// </summary>
+        Dictionary<string, IHandler> UberLoggerCLI_Subcommands { get; }
+
+        /// <summary>
+        /// May be null, in which case this command has no direct action, only
+        /// subcommands.
+        /// </summary>
+        Action<string[]> UberLoggerCLI_Action { get; }
+    }
+
+
+    public class Handler : IHandler
+    {
+        protected readonly string CommandName;
+
+        protected readonly Dictionary<string, IHandler> Subcommands = new Dictionary<string, IHandler>();
+
+        protected readonly Action<string[]> Action;
+
+        public Handler(string cmdName, IEnumerable<Handler> subhandlers = null, Action<string[]> action = null)
+        {
+            CommandName = cmdName;
+            Action = action;
+
+            if (subhandlers != null)
+            {
+                foreach (var h in subhandlers)
+                {
+                    AddHandler(h);
+                }
+            }
+        }
+
+        public virtual string UberLoggerCLI_CommandName => CommandName;
+
+        public virtual Dictionary<string, IHandler> UberLoggerCLI_Subcommands => Subcommands;
+
+        public virtual Action<string[]> UberLoggerCLI_Action => Action;
+
+
+        public void AddHandler(IHandler h)
+        {
+            Subcommands[h.UberLoggerCLI_CommandName] = h;
+        }
+
+        public void RemoveHandler(IHandler h)
+        {
+            Subcommands.Remove(h.UberLoggerCLI_CommandName);
+        }
+
+        public void RemoveHandlerByName(string n)
+        {
+            Subcommands.Remove(n);
+        }
+    }
+
+
+    class ReflectiveHandler : IHandler
+    {
+        readonly object Object;
+        readonly MethodInfo Method;
+
+        internal ReflectiveHandler(object o, string cmdName)
+        {
+            Assert.IsNotNull(o);
+            Assert.IsNotNull(cmdName);
+
+            Object = o;
+            UberLoggerCLI_CommandName = cmdName;
+
+            var t = o.GetType();
+            Method = t.GetMethod(CallbackMethodName, BindingFlags.NonPublic | BindingFlags.Instance);
+            Assert.IsNotNull(Method);
+        }
+
+        public string UberLoggerCLI_CommandName { get; }
+
+        public Dictionary<string, IHandler> UberLoggerCLI_Subcommands => null;
+
+        public Action<string[]> UberLoggerCLI_Action =>
+            (string[] bits) => Method.Invoke(Object, new object[] { bits });
+    }
+
+
+    readonly Handler topHandler;
     string commandText = "";
 
     bool focusNextRedraw;
 
 
-    void Start()
+    UberLoggerCLI()
     {
-        RegisterHandler(this, "help");
+        topHandler = new Handler("", null, _ => LogTopLevelCommands());
     }
 
+
+    public void RegisterHandler(IHandler h)
+    {
+        topHandler.AddHandler(h);
+    }
+
+    public void UnregisterHandler(IHandler h)
+    {
+        topHandler.RemoveHandler(h);
+    }
+
+    public void RegisterHandler(string cmdName, object subcommands)
+    {
+        var handler = MakeHandler(cmdName, subcommands);
+        topHandler.AddHandler(handler);
+    }
+
+    public void UnregisterHandler(string cmdName)
+    {
+        topHandler.RemoveHandlerByName(cmdName);
+    }
+
+    IHandler MakeHandler(string cmdName, object subcommands)
+    {
+        var action = subcommands as Action<string[]>;
+        var dict = subcommands as Dictionary<string, object>;
+
+        var result = new Handler(cmdName, null, action);
+        if (dict != null)
+        {
+            foreach (var c in dict)
+            {
+                var h = MakeHandler(c.Key, c.Value);
+                result.AddHandler(h);
+            }
+        }
+        return result;
+    }
 
     public void RegisterHandler(Component c, string cmdName = null)
     {
         cmdName = CommandName(c, cmdName);
-        handlers[cmdName] = c;
+        RegisterHandler(new ReflectiveHandler(c, cmdName));
     }
 
     public void UnregisterHandler(Component c, string cmdName = null)
     {
         cmdName = CommandName(c, cmdName);
-        handlers.Remove(cmdName);
+        topHandler.RemoveHandlerByName(cmdName);
     }
 
     string CommandName(Component c, string cmdName)
@@ -84,31 +217,56 @@ public sealed class UberLoggerCLI : MonoBehaviour {
             return;
         }
         var bits = cmd.Split(' ');
-        var handlerName = bits[0];
-        Component handler;
-        if (handlers.TryGetValue(handlerName, out handler) && handler != null)
+        var idx = 0;
+        IHandler handler = topHandler;
+        while (true)
         {
-            var t = handler.GetType();
-            var mi = t.GetMethod(CallbackMethodName, BindingFlags.NonPublic | BindingFlags.Instance);
-            mi.Invoke(handler, new object[] { bits });
+            if (idx < bits.Length)
+            {
+                var handlerName = bits[idx];
+                IHandler subhandler;
+                var subcmds = handler.UberLoggerCLI_Subcommands;
+                if (subcmds != null && subcmds.TryGetValue(handlerName, out subhandler))
+                {
+                    handler = subhandler;
+                    idx++;
+                    continue;
+                }
+            }
+
+            var action = handler.UberLoggerCLI_Action;
+            if (action == null)
+            {
+                LogHandlerUsage(handler);
+            }
+            else
+            {
+                action(bits);
+            }
+            return;
+        }
+    }
+
+
+    void LogTopLevelCommands()
+    {
+        var commands = topHandler.UberLoggerCLI_Subcommands.Keys.OrderBy(x => x);
+        Debug.LogFormat("Known commands: {0}", string.Join(", ", commands));
+    }
+
+
+    public static void LogHandlerUsage(IHandler handler)
+    {
+        var hs = handler.UberLoggerCLI_Subcommands;
+        if (hs == null || hs.Count == 0)
+        {
+            Debug.LogFormat("Command {0} takes no arguments", handler.UberLoggerCLI_CommandName);
         }
         else
         {
-            Debug.LogFormat("No such command handler: {0}", handlerName);
+            var subcommands = hs.Keys;
+            LogSubcommandUsage(handler.UberLoggerCLI_CommandName, subcommands);
         }
-    }
-
-
-    void UberLoggerCLI_HandleCommand()
-    {
-        var handlerNames = handlers.Keys.OrderBy(x => x);
-        LogCommandUsage(handlerNames);
-    }
-
-
-    public static void LogCommandUsage(IEnumerable<string> commands)
-    {
-        Debug.LogFormat("Known commands: {0}", string.Join(", ", commands));
     }
 
 
